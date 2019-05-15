e02f44e7b271efb8c822edc395eb54cbd4095bcb
TEACHER <kellyfire611@gmail.com>
2019-05-15 18:43:04 +0700

    Thêm phân trang Sản phẩm

--- php/twig/backend/sanpham/index.php
+++ php/twig/backend/sanpham/index.php
@@ -4,6 +4,8 @@ require_once __DIR__.'/../../bootstrap.php';
 // Truy vấn database để lấy danh sách
 // 1. Include file cấu hình kết nối đến database, khởi tạo kết nối $conn
 include_once(__DIR__.'/../../dbconnect.php');
+include_once(__DIR__ . '/../../Paginator.php');
+
 // 2. Chuẩn bị câu truy vấn $sql
 // Sử dụng HEREDOC của PHP để tạo câu truy vấn SQL với dạng dễ đọc, thân thiện với việc bảo trì code
 $sql = <<<EOT
@@ -19,12 +21,21 @@ $sql = <<<EOT
 EOT;
 
 // 3. Thực thi câu truy vấn SQL để lấy về dữ liệu
-$result = mysqli_query($conn, $sql);
+$limit      = (isset($_GET['limit'])) ? $_GET['limit'] : 5;
+$page       = (isset($_GET['page'])) ? $_GET['page'] : 1;
+$paginator  = new Paginator($twig, $conn, $sql);
+$data       = $paginator->getData($limit, $page);
+$data = $data->data;
+//dd($data);
+
+//$result = mysqli_query($conn, $sql);
 // 4. Khi thực thi các truy vấn dạng SELECT, dữ liệu lấy về cần phải phân tích để sử dụng
 // Thông thường, chúng ta sẽ sử dụng vòng lặp while để duyệt danh sách các dòng dữ liệu được SELECT
 // Ta sẽ tạo 1 mảng array để chứa các dữ liệu được trả về
-$data = [];
-while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+// $data = [];
+// while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
+$formatedData = [];
+foreach($data as $row)
 {
     $km_tomtat = '';
     if(!empty($row['km_ten'])) {
@@ -37,7 +48,7 @@ while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
             date('d/m/Y', strtotime($row['km_tungay'])),    //vd: '2019-04-25'
             date('d/m/Y', strtotime($row['km_denngay'])));  //vd: '2019-05-10'
     }
-    $data[] = array(
+    $formatedData[] = array(
         'sp_ma' => $row['sp_ma'],
         'sp_ten' => $row['sp_ten'],
         // Sử dụng hàm number_format(số tiền, số lẻ thập phân, dấu phân cách số lẻ, dấu phân cách hàng nghìn) để định dạng số khi hiển thị trên giao diện. Vd: 15800000 -> format thành 15,800,000.66 vnđ
@@ -56,6 +67,10 @@ while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
         'km_tomtat' => $km_tomtat,
     );
 }
+// dd($formatedData);
 // Yêu cầu `Twig` vẽ giao diện được viết trong file `backend/sanpham/index.html.twig`
 // với dữ liệu truyền vào file giao diện được đặt tên là `ds_sanpham`
-echo $twig->render('backend/sanpham/index.html.twig', ['ds_sanpham' => $data] );
+echo $twig->render('backend/sanpham/index.html.twig', [
+    'ds_sanpham' => $formatedData,
+    'paginator' => $paginator
+] );
