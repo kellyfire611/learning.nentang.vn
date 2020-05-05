da2e4770ab558d43b12f8f3e17571008ba2404f9
phucuong <phucuong@ctu.edu.vn>
2020-05-05 18:02:32 +0700

    Bổ sung FontAwesome cho Trang chủ

--- php/simple/index.php
+++ php/simple/index.php
@@ -30,6 +30,9 @@
 
     <!-- Liên kết JS Bootstrap bằng CDN -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <!-- Liên kết JS FontAwesome bằng CDN -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
 </body>
 
 </html>
